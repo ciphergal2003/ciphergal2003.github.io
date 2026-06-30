b760ac8cbcc831e8f04faa63233e24883f183702
ciphergal2003 <jessica.davies2003@outlook.com>
2026-06-30 21:48:37 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from flask import Flask, jsonify, request
-
-app = Flask(__name__)
+from json import load
+from flask_cors import CORS
 
 data = {
-    "error": "No error",
-    "name": "Jessica",
-    "age": "22"
+    "arithmetic": {
+        "q1": {
+            "question": "What is 100/25?",
+            "options": ["3", "4", "5", "6", "7"],
+            "correctIndex": 1
+        },
+        
+    },
+    "algebra": {
+        "q1": {
+            "question": "Find the value of X: '2x = 16'",
+            "options": ["4", "6", "8", "10", "12"],
+            "correctIndex": 2
+        }
+    }
 }
+
+PORT = 7777
+
+app = Flask(__name__)
+CORS(app)
 
 error = {
     "error": "Invalid access key."
@@ -14,17 +31,23 @@ error = {
 
 @app.route("/")
 def index():
-    return "Go to /api!"
+    return "<h1>Go to /api!<h1>"
 
-@app.route("/api")
+@app.route("/mathsapi")
 def api():
+
     passed_data = request.args.to_dict()
     given_key = passed_data["accesskey"]
 
-    if given_key == "Jessica2003":
+    if given_key == "ILoveMath":
+        # with open("data.json") as f:
+        #     myData = load(f)
+        # return jsonify(myData)
+
         return jsonify(data)
     else:
         return jsonify(error)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=PORT)
+
